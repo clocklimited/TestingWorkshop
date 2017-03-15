e7350d6533d1b5453264e4759071115001eb44ab
Paul Serby <paul.serby@clock.co.uk>
2017-03-15 19:30:02 +0000

    Exercise 1

--- exercise/1-hello-world/HelloWorld.test.js
+++ exercise/1-hello-world/HelloWorld.test.js
@@ -1,10 +1,14 @@
-// import React from 'react'
-// import assert from 'assert'
-// import { shallow } from 'enzyme'
-// import HelloWorld from './HelloWorld'
+import React from 'react'
+import assert from 'assert'
+import { shallow } from 'enzyme'
+import HelloWorld from './HelloWorld'
 
 describe('Exercise 1: Component: HelloWorld', function () {
   it('should say `Hello World!`', function () {
-    // const wrapper = shallow(<HelloWorld />)
+    const wrapper = shallow(<HelloWorld />)
+    assert.equal(wrapper.find('p').text(), 'Hello World!')
+    assert.equal(wrapper.html(), '<p>Hello World!</p>')
+    assert.equal(wrapper.type(), 'p')
+    assert.equal(wrapper.text(), 'Hello World!')
   })
 })
