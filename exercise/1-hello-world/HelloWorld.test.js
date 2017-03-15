0f509cb0cf5e48a7794635e3979acb78b3f0f9dd
Paul Serby <paul.serby@clock.co.uk>
2017-03-15 19:28:35 +0000

    Blank test

---
+++ exercise/1-hello-world/HelloWorld.test.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import assert from 'assert'
+// import { shallow } from 'enzyme'
+// import HelloWorld from './HelloWorld'
+
+describe('Exercise 1: Component: HelloWorld', function () {
+  it('should say `Hello World!`', function () {
+    // const wrapper = shallow(<HelloWorld />)
+  })
+})
